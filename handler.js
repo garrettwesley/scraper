9702929dab0ecba65abb1d35bf44b135eaa1e8c8
Garrett Wesley <garrettwesley@berkeley.edu>
2019-01-07 01:08:39 -0800

    text

--- handler.js
+++ handler.js
@@ -1,11 +1,22 @@
 const request = require('axios');
 const { formatData } = require('./helpers')
+const TO_NUMBER = '16198707007'
+const FROM_NUMBER = '14123873196'
 
 module.exports.getData = (event, context, callback) => {
     request('https://classes.berkeley.edu/content/2019-spring-compsci-61a-001-lec-001')
         .then(({ data }) => {
-            const formatted = formatData(data);
-            callback(null, formatted);
+            const res = formatData(data);
+            if (res.length) {
+              var nexmo = new Nexmo({
+                apiKey: process.env.NEXMO_API_KEY,
+                apiSecret: process.env.NEXMO_API_SECRET
+              });
+              nexmo.message.sendSms(FROM_NUMBER, TO_NUMBER,
+                    `${res.class} has ${res.enrolled}/${res.maxEnroll} already enrolled with
+                    already ${res.waitlisted}/${res.waitlistedCount} waitlisted.`);
+            }
+            callback(null, res);
         })
         .catch(callback);
 };
--- helpers.js
+++ helpers.js
@@ -3,11 +3,11 @@ const cheerio = require('cheerio')
 function formatData(html) {
     const $ = cheerio.load(html)
     const data = $('.handlebarData').data('enrollment').available.enrollmentStatus
-    let values = {  class:           'CS 61a',
-                    enrolledCount:   data.enrolledCount,
-                    maxEnroll:       data.maxEnroll,
-                    waitlistedCount: data.waitlistedCount,
-                    maxWaitlist:     data.maxWaitlist
+    let values = {  class:       'CS 61a',
+                    enrolled:    data.enrolledCount,
+                    maxEnroll:   data.maxEnroll,
+                    waitlisted:  data.waitlistedCount,
+                    maxWaitlist: data.maxWaitlist
                 }
 
     return values
